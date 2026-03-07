4d859e4017518259387586b01605a5df6a21a1ee
Justin King <getafixx149@gmail.com>
2026-03-07 20:46:16 +0100

    Routes for contact

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+|
+*/
+
+Route::get('/', fn () => view('welcome'));
+
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
