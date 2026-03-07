ffbaca8b87fd609042b331e969c13e4cc975621d
Justin King <getafixx149@gmail.com>
2026-03-07 20:46:58 +0100

    formatting and fixes

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,15 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function testThatTrueIsTrue(): void
     {
         $this->assertTrue(true);
     }
